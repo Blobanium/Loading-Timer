731bcc339bff7626537887c1cc7e0a9ade13e99f
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-02-16 12:58:44 -0800

    Close #1 and add Initialize and Fully load Loggers

--- src/main/java/net/fabricmc/example/LoadingTimer.java
+++ src/main/java/net/fabricmc/example/LoadingTimer.java
@@ -6,21 +6,37 @@ public class LoadingTimer implements ModInitializer {
 	public static long timeToLoad;
 	public static final long startingTime = System.currentTimeMillis();
 	private static final long STARTINGTIME2 = startingTime;
+	public static int hasGameStarted = 0;
 	
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 		System.out.println("Loading Timer Initialized");
 	}
 
 	public static void load() {
-		final long timeToLoad = System.currentTimeMillis() - STARTINGTIME2;
-		final long tTLDeductor = timeToLoad/1000;
-		final double beforeFinalResult = (timeToLoad - (tTLDeductor * 1000));
-		final double finalResult = tTLDeductor + (beforeFinalResult / 1000);
-		System.out.println("Minecraft took " + finalResult + " seconds to load");
+		// The "Load" Procedure Runs twice, one for initialization and the other for loading completely
+		// This is controlled by the variable called "hasGameStarted"
+		if(hasGameStarted == 0) {
+			hasGameStarted = 1;
+			final long timeToLoad = System.currentTimeMillis() - STARTINGTIME2;
+			final long tTLDeductor = timeToLoad/1000;
+			final double beforeFinalResult = (timeToLoad - (tTLDeductor * 1000));
+			final double finalResult = tTLDeductor + (beforeFinalResult / 1000);
+			System.out.println("Minecraft took " + finalResult + " seconds to initialize");
+		} else {
+			if(hasGameStarted == 1) {
+				hasGameStarted = 2;
+				final long timeToLoad = System.currentTimeMillis() - STARTINGTIME2;
+				final long tTLDeductor = timeToLoad/1000;
+				final double beforeFinalResult = (timeToLoad - (tTLDeductor * 1000));
+				final double finalResult = tTLDeductor + (beforeFinalResult / 1000);
+				System.out.println("Minecraft took " + finalResult + " seconds to Fully Load");
+			}
+		}
+		// Throw An Exception if the Variable hasGameStarted is out of range
+		if(!(hasGameStarted == 1 | hasGameStarted == 2)){
+			throw new IndexOutOfBoundsException("Variable hasGameStarted is out of range! Value of hasGameStarted is" + hasGameStarted + " ");
+		}
 	}
 }
 
