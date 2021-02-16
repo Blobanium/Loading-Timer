72b21701c30f411cf489162c39ebe2235688ffee
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-02-15 16:34:46 -0800

    Base

---
+++ src/main/java/net/fabricmc/example/LoadingTimer.java
@@ -0,0 +1,26 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LoadingTimer implements ModInitializer {
+	public static long timeToLoad;
+	public static final long startingTime = System.currentTimeMillis();
+	private static final long STARTINGTIME2 = startingTime;
+	
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		System.out.println("System time is " + System.currentTimeMillis());
+	}
+
+	public static void load() {
+		final long timeToLoad = System.currentTimeMillis() - STARTINGTIME2;
+		final long tTLDeductor = timeToLoad/1000;
+		final double wTF = (timeToLoad - (tTLDeductor * 1000));
+		final double finalResult = tTLDeductor + (wTF / 1000);
+		System.out.println("Minecraft took " + finalResult + " seconds to load");
+	}
+}
+
