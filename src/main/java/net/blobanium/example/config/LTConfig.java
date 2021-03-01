ded448ad2db006f91e79ce950c5360d326d5e503
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-02-28 18:52:25 -0800

    Implement the AutoConfig register lines without having it crash

--- src/main/java/net/blobanium/example/config/LTConfig.java
+++ src/main/java/net/blobanium/example/config/LTConfig.java
@@ -8,6 +8,16 @@ import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.ConfigHolder;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import me.shedaniel.autoconfig.util.fabric.UtilsImpl;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.google.gson.Gson;
 
 /* ATTENTION!
 // This Is The Example from the autoconfig wiki by shedaniel
@@ -15,8 +25,11 @@ import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 // Once i get the config working i will change this class
 */
 
+
+
 public class LTConfig {
-    @Config(name = "loading-timer")
+
+    @Config(name = "loadingtimer")
     public class ModConfig implements ConfigData {
         boolean toggleA = true;
         boolean toggleB = false;
@@ -34,5 +47,14 @@ public class LTConfig {
     }
 
     public static void init() {
+        try {
+            ConfigHolder<ModConfig> holder = AutoConfig.register(ModConfig.class, (GsonConfigSerializer::new));
+            ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+        } catch (RuntimeException e) {
+            Path LTConfigPath = UtilsImpl.getConfigFolder().resolve("loadingtimer.json");
+            File LTConfigFile = LTConfigPath.toFile();
+            System.out.println("A Config File Would have been Created Here But Currently the code for it does not exist yet. It is a still a WIP");
+            //TODO: Find a way to generate a file
+        }
     }
 }
