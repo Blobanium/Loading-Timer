8866c489d8696aabdff6d3d36fe6fbcff6e547d3
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-03-01 21:32:01 -0800

    CONFIG GENERATION FINALLY

--- src/main/java/net/blobanium/example/config/LTConfig.java
+++ src/main/java/net/blobanium/example/config/LTConfig.java
@@ -11,6 +11,8 @@ import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import me.shedaniel.autoconfig.util.fabric.UtilsImpl;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /* ATTENTION!
 // This Is The Example from the autoconfig wiki by shedaniel
@@ -44,10 +46,25 @@ public class LTConfig {
             ConfigHolder<ModConfig> holder = AutoConfig.register(ModConfig.class, (GsonConfigSerializer::new));
             ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
         } catch (RuntimeException e) {
-            File file = new File(UtilsImpl.getConfigFolder() + "/loadingtimer.json");
-            System.out.println(file);
+            createFile();
+            ConfigHolder<ModConfig> holder = AutoConfig.register(ModConfig.class, (GsonConfigSerializer::new));
+            ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
             System.out.println("A Config File Would have been Created Here But Currently the code for it does not exist yet. It is a still a WIP");
             //TODO: Find a way to generate a .json file using gson
+        }
+    }
+
+    public static void createFile() {
+        File file = new File(UtilsImpl.getConfigFolder() + "/loadingtimer.json");
+        System.out.println(file);
+        System.out.println(UtilsImpl.getConfigFolder());
+        try{
+            FileWriter myWriter = new FileWriter(UtilsImpl.getConfigFolder() + "/loadingtimer.json");
+            myWriter.write("{ }");
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("Something caused Config Creation to fail");
+            e.printStackTrace();
         }
     }
 }
