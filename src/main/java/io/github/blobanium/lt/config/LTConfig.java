5037e479066e3f7d21d601e23cf5c5df4b8ffdf9
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-03-03 20:19:45 -0800

    Bump Mod Version and added more details

--- src/main/java/io/github/blobanium/lt/config/LTConfig.java
+++ src/main/java/io/github/blobanium/lt/config/LTConfig.java
@@ -20,6 +20,7 @@ public class LTConfig {
     public static void init() {
         try {
             System.out.println("The Config has been temporarily disabled due to a bug and will be fixed later");
+            System.out.println("For More Information, Check https://github.com/Blobanium/Loading-Timer/issues/7");
         } catch (RuntimeException e) {
             System.out.println("Configuration file not found! Creating...");
             createFile();
