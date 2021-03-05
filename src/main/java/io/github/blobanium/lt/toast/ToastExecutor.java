b34cdae05f39bf062312c0f92ce6b73eccb31cda
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-03-05 11:35:27 -0800

    Remove unused repos, And maybe add an Easter egg

--- src/main/java/io/github/blobanium/lt/toast/ToastExecutor.java
+++ src/main/java/io/github/blobanium/lt/toast/ToastExecutor.java
@@ -6,8 +6,18 @@ import net.minecraft.text.LiteralText;
 
 public class ToastExecutor {
     public static void executeToast(double toastTimeValue){
-        SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
-        new LiteralText("Loading Timer"), new LiteralText("Minecraft took " + toastTimeValue + " seconds to load"));
-        MinecraftClient.getInstance().getToastManager().add(toast);
+        //Shhhhh!!
+        short min = 1;
+        short max = 1000;
+        long random_double = Math.round(Math.random() * (max - min + 1) + min);
+        if(random_double == 100){
+            SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
+            new LiteralText("Laoding Timer"), new LiteralText("Minecraft took " + toastTimeValue + " seconds to load"));
+            MinecraftClient.getInstance().getToastManager().add(toast);
+        } else {
+            SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
+            new LiteralText("Loading Timer"), new LiteralText("Minecraft took " + toastTimeValue + " seconds to load"));
+            MinecraftClient.getInstance().getToastManager().add(toast);
+        }
     }
 }
