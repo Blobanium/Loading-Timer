a1000cbf7dd01efd310abe49f1c1e5bf9465ae95
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-04-01 11:58:35 -0700

    add MinecraftClientGameMixin

---
+++ src/main/java/io/github/blobanium/lt/mixin/MinecraftClientGameMixin.java
@@ -0,0 +1,14 @@
+package io.github.blobanium.lt.mixin;
+
+import net.minecraft.client.MinecraftClientGame;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClientGame.class)
+public class MinecraftClientGameMixin {
+    @Inject(at = @At("HEAD"), method = "onStartGameSession ()V")
+    private void onStartGameSession(CallbackInfo info) {
+    }
+}
