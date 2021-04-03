eb19166fbcb146fa8db14a21fe82af52683c115e
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-04-03 11:45:11 -0700

    add a comment in MinecraftClientGameMixin.java

--- src/main/java/io/github/blobanium/lt/mixin/MinecraftClientGameMixin.java
+++ src/main/java/io/github/blobanium/lt/mixin/MinecraftClientGameMixin.java
@@ -10,5 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MinecraftClientGameMixin {
     @Inject(at = @At("HEAD"), method = "onStartGameSession ()V")
     private void onStartGameSession(CallbackInfo info) {
+        //im gonna put something here soon
     }
 }
