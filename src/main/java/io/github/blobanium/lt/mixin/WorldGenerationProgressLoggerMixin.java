9de0c15d0d704565ad49e518bde8aef778ca8086
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-04-12 16:54:58 -0700

    Convert an accessor into a mixin
    bc accessors didnt seem to work. try a different one for now. It still crashes when you try to go into singleplayer.

---
+++ src/main/java/io/github/blobanium/lt/mixin/WorldGenerationProgressLoggerMixin.java
@@ -0,0 +1,22 @@
+package io.github.blobanium.lt.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+import net.minecraft.server.WorldGenerationProgressLogger;
+
+//how to convert a accessor into a mixin
+@Mixin(WorldGenerationProgressLogger.class)
+public class WorldGenerationProgressLoggerMixin {
+    @Inject(at = @At("HEAD"), method = "start")
+    private void start(){
+        System.out.println("start");
+    }
+
+    @Inject(at = @At("HEAD"), method = "stop")
+    private void stop(){
+        System.out.println("stop");
+    }
+
+}
