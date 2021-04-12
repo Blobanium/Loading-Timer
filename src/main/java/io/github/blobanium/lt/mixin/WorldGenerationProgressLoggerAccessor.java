3bc3d227d83a28a67939ddee5025adced5c521ce
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-04-11 17:54:20 -0700

    Update WorldGenerationProgressLoggerAccessor.java

--- src/main/java/io/github/blobanium/lt/mixin/WorldGenerationProgressLoggerAccessor.java
+++ src/main/java/io/github/blobanium/lt/mixin/WorldGenerationProgressLoggerAccessor.java
@@ -3,10 +3,13 @@ package io.github.blobanium.lt.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.server.WorldGenerationProgressLogger;
 
 @Mixin(WorldGenerationProgressLogger.class)
 public interface WorldGenerationProgressLoggerAccessor {
     @Accessor
     long getStartTime();
+
+    public static long startTime = ((WorldGenerationProgressLoggerAccessor) MinecraftClient.getInstance()).getStartTime();
 }
