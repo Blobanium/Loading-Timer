d6831073f5a907d7339b01741b98887841926678
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-04-13 12:07:09 -0700

    Move Everything from WorldLoadingTime To The Mixin

--- src/main/java/io/github/blobanium/lt/mixin/WorldGenerationProgressLoggerMixin.java
+++ src/main/java/io/github/blobanium/lt/mixin/WorldGenerationProgressLoggerMixin.java
@@ -7,17 +7,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.server.WorldGenerationProgressLogger;
 
-import io.github.blobanium.lt.world.WorldLoadingTime;
+import io.github.blobanium.lt.LoadingTimer;
+import io.github.blobanium.lt.util.math.MathUtil;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mixin(WorldGenerationProgressLogger.class)
 public class WorldGenerationProgressLoggerMixin {
+
+    public static long worldStartingTime;
+    public static final Logger LOGGER = LogManager.getLogger("Loading Timer");
+
     @Inject(at = @At("HEAD"), method = "start")
     private void start(CallbackInfo ci){
-        WorldLoadingTime.start();
+        if(LoadingTimer.worldLoadTime){
+            if(LoadingTimer.insanePrecision){
+                worldStartingTime = System.nanoTime();
+            } else {
+                worldStartingTime = System.currentTimeMillis();
+            }
+        }
     }
 
     @Inject(at = @At("HEAD"), method = "stop")
     private void stop(CallbackInfo ci){
-        WorldLoadingTime.stop();
+        double worldTime = MathUtil.calculateMain(worldStartingTime);
+        LOGGER.info(worldTime);
     }
 }
