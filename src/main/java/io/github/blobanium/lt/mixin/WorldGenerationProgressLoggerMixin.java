a6682cd2246292450dbe82f9ab71180c3e1d8898
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-04-13 11:03:24 -0700

    Finally fixed on why that mixin was causing crashes.
    turns out i didnt put in Callback info so yeah

--- src/main/java/io/github/blobanium/lt/mixin/WorldGenerationProgressLoggerMixin.java
+++ src/main/java/io/github/blobanium/lt/mixin/WorldGenerationProgressLoggerMixin.java
@@ -3,6 +3,7 @@ package io.github.blobanium.lt.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.server.WorldGenerationProgressLogger;
 
@@ -10,12 +11,12 @@ import net.minecraft.server.WorldGenerationProgressLogger;
 @Mixin(WorldGenerationProgressLogger.class)
 public class WorldGenerationProgressLoggerMixin {
     @Inject(at = @At("HEAD"), method = "start")
-    private void start(){
+    private void start(CallbackInfo ci){
         System.out.println("Start");
     }
 
     @Inject(at = @At("HEAD"), method = "stop")
-    private void stop(){
+    private void stop(CallbackInfo ci){
         System.out.println("Stop");
     }
 }
