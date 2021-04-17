72919e0672d2cb48ecdbbd73b5848e132d693318
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-04-17 10:58:47 -0700

    Add Toast

--- src/main/java/io/github/blobanium/lt/util/resource/ResourceLoadingTimer.java
+++ src/main/java/io/github/blobanium/lt/util/resource/ResourceLoadingTimer.java
@@ -1,6 +1,7 @@
 package io.github.blobanium.lt.util.resource;
 
 import io.github.blobanium.lt.LoadingTimer;
+import io.github.blobanium.lt.toast.ToastExecutor;
 import io.github.blobanium.lt.util.math.MathUtil;
 
 import org.apache.logging.log4j.LogManager;
@@ -9,6 +10,7 @@ import org.apache.logging.log4j.Logger;
 public class ResourceLoadingTimer {
     public static long resourceStartingTime;
     private static final Logger LOGGER = LogManager.getLogger("Loading Timer");
+    public static double resourceResult;
 
     public static void startTimer(){
         if(LoadingTimer.insanePrecision){
@@ -19,6 +21,10 @@ public class ResourceLoadingTimer {
     }
 
     public static void stopTimer(){
-        LOGGER.info("Resource Loading Time: " + MathUtil.calculateMain(resourceStartingTime) + " seconds");
+        resourceResult = MathUtil.calculateMain(resourceStartingTime);
+        LOGGER.info("Resource Loading Time: " + resourceResult + " seconds");
+        if(LoadingTimer.resourceLoadNotif){
+            ToastExecutor.executeToast(resourceResult, 3);
+        }
     }
 }
