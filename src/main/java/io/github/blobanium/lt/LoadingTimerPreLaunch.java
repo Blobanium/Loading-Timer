5777513a9693c076152c0f9076a217b64efff701
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-04-20 12:08:47 -0700

    Have Loading Timer Run off of literal text if Fabric API is not detected

--- src/main/java/io/github/blobanium/lt/LoadingTimerPreLaunch.java
+++ src/main/java/io/github/blobanium/lt/LoadingTimerPreLaunch.java
@@ -1,12 +1,26 @@
 package io.github.blobanium.lt;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class LoadingTimerPreLaunch implements PreLaunchEntrypoint {
+    public static final Logger LOGGER = LogManager.getLogger("Loading Timer");
+
     @Override
     public void onPreLaunch(){
         LoadingTimer.startingTime = System.currentTimeMillis();
         LoadingTimer.startingTimeNano = System.nanoTime();
+        checkCompat();
         LoadingTimer.configRegister();
+    }
+
+    public static void checkCompat(){
+        if(!FabricLoader.getInstance().isModLoaded("fabric")){
+            LOGGER.warn("Fabric API was not detected");
+            LOGGER.warn("Translations will not work at all.");
+        }
     }
 }
--- src/main/java/io/github/blobanium/lt/toast/ToastExecutor.java
+++ src/main/java/io/github/blobanium/lt/toast/ToastExecutor.java
@@ -1,7 +1,10 @@
 package io.github.blobanium.lt.toast;
 
+import net.fabricmc.loader.api.FabricLoader;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.toast.SystemToast;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 
 import org.apache.logging.log4j.LogManager;
@@ -13,33 +16,64 @@ public class ToastExecutor {
     public static final Logger LOGGER = LogManager.getLogger("Loading Timer");
 
     public static void executeToast(double toastTimeValue, int messageToastSelector){
-        if(messageToastSelector == 1){
-            LOGGER.debug("Showing Toast Notification");
-            //Shhhhh!!
-            short min = 1;
-            short max = 1000;
-            long random_double = Math.round(Math.random() * (max - min + 1) + min);
-            if(random_double == 100){
+        if(FabricLoader.getInstance().isModLoaded("fabric")){
+            if(messageToastSelector == 1){
+                LOGGER.debug("Showing Toast Notification");
+                //Shhhhh!!
+                short min = 1;
+                short max = 1000;
+                long random_double = Math.round(Math.random() * (max - min + 1) + min);
+                if(random_double == 100){
+                    SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
+                    new TranslatableText("loading-timer.title_ee"), new TranslatableText("loading-timer.message_text", toastTimeValue));
+                    MinecraftClient.getInstance().getToastManager().add(toast);
+                } else {
+                    SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
+                    new TranslatableText("loading-timer.title"), new TranslatableText("loading-timer.message_text", toastTimeValue));
+                    MinecraftClient.getInstance().getToastManager().add(toast);
+                }
+            }
+
+            if(messageToastSelector == 2){
                 SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
-                new TranslatableText("loading-timer.title_ee"), new TranslatableText("loading-timer.message_text", toastTimeValue));
-                MinecraftClient.getInstance().getToastManager().add(toast);
-            } else {
-                SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
-                new TranslatableText("loading-timer.title"), new TranslatableText("loading-timer.message_text", toastTimeValue));
+                new TranslatableText("loading-timer.title"), new TranslatableText("loading-timer.world_message_text", toastTimeValue));
                 MinecraftClient.getInstance().getToastManager().add(toast);
             }
-        }
 
-        if(messageToastSelector == 2){
-            SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
-            new TranslatableText("loading-timer.title"), new TranslatableText("loading-timer.world_message_text", toastTimeValue));
-            MinecraftClient.getInstance().getToastManager().add(toast);
-        }
+            if(messageToastSelector == 3){
+                SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
+                new TranslatableText("loading-timer.title"), new TranslatableText("loading-timer.resource_loading_text", toastTimeValue));
+                MinecraftClient.getInstance().getToastManager().add(toast);
+            }
+        } else {
+            if(messageToastSelector == 1){
+                LOGGER.debug("Showing Toast Notification");
+                //Shhhhh!!
+                short min = 1;
+                short max = 1000;
+                long random_double = Math.round(Math.random() * (max - min + 1) + min);
+                if(random_double == 100){
+                    SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
+                    new LiteralText("Laoding Timer"), new LiteralText("Minecraft took " + toastTimeValue + " seconds to load"));
+                    MinecraftClient.getInstance().getToastManager().add(toast);
+                } else {
+                    SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
+                    new LiteralText("Loading Timer"), new LiteralText("Minecraft took " + toastTimeValue + " seconds to load"));
+                    MinecraftClient.getInstance().getToastManager().add(toast);
+                }
+            }
 
-        if(messageToastSelector == 3){
-            SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
-            new TranslatableText("loading-timer.title"), new TranslatableText("loading-timer.resource_loading_text", toastTimeValue));
-            MinecraftClient.getInstance().getToastManager().add(toast);
+            if(messageToastSelector == 2){
+                SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
+                new LiteralText("Loading Timer"), new LiteralText("It took " + toastTimeValue + " seconds to load into/generate this world"));
+                MinecraftClient.getInstance().getToastManager().add(toast);
+            }
+
+            if(messageToastSelector == 3){
+                SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT,
+                new LiteralText("Loading Timer"), new LiteralText("it took approximately " + toastTimeValue + " seconds to load the resource pack(s)"));
+                MinecraftClient.getInstance().getToastManager().add(toast);
+            }
         }
 
         if(!(messageToastSelector >= 1 && messageToastSelector <= 3)){
