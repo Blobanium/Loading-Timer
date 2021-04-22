74f2af1790f662ed6a30eb2e46bfc49aa164a2e0
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-04-22 11:41:45 -0700

    ClothConfig and Modmenu Implmentation
    Hope this works this time

---
+++ src/main/java/io/github/blobanium/lt/config/ClothConfig.java
@@ -0,0 +1,9 @@
+package io.github.blobanium.lt.config;
+
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import net.minecraft.text.TranslatableText;
+
+public class ClothConfig {
+    ConfigBuilder builder = ConfigBuilder.create().setTitle(new TranslatableText("loading-timer.config"));
+    
+}
