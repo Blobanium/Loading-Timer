18778fd9938cc54af4142b304db09cc440577534
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-04-24 14:35:19 -0700

    Fix some bugs with new loop

--- src/main/java/io/github/blobanium/lt/experimental/LoadLoopV2.java
+++ src/main/java/io/github/blobanium/lt/experimental/LoadLoopV2.java
@@ -5,31 +5,38 @@ import io.github.blobanium.lt.util.math.MathUtil;
 import net.minecraft.client.MinecraftClient;
 
 public class LoadLoopV2 {
+    public static boolean advanceLoop = true;
+
     public static void load2(){
     	LoadingTimer.finalResult = MathUtil.calculateMain(LoadingTimer.STARTINGTIME2);
-    
-    	if (LoadingTimer.hasGameStarted == 0) {
+        advanceLoop = true;
+
+    	if (LoadingTimer.hasGameStarted == 0 && advanceLoop) {
     		LoadingTimer.hasGameStarted = 1;
     		if (MinecraftClient.getInstance().options.fullscreen) {
     			LoadingTimer.isClientFullscreen = true;
     			LoadingTimer.isClientFullscreen2 = true;
     		}
     		LoadingTimer.getDimensions();
+            advanceLoop = false;
     	}
     
     	if(LoadingTimer.resV == MinecraftClient.getInstance().currentScreen.height && LoadingTimer.resH == MinecraftClient.getInstance().currentScreen.width){
-    		if(LoadingTimer.hasGameStarted == 1){
+    		if(LoadingTimer.hasGameStarted == 1 && advanceLoop){
     			if(LoadingTimer.isClientFullscreen){
     				LoadingTimer.hasGameStarted = 2;
+                    advanceLoop = false;
     			} else {
     				LoadingTimer.hasGameStarted = 3;
     				LoadingTimer.lastMessage();
+                    advanceLoop = false;
     			}
     		}
     
-    		if(LoadingTimer.hasGameStarted == 2){
+    		if(LoadingTimer.hasGameStarted == 2 && advanceLoop){
     			LoadingTimer.hasGameStarted = 3;
     			LoadingTimer.lastMessage();
+                advanceLoop = false;
     		}
     
     	} else {
@@ -44,5 +51,4 @@ public class LoadLoopV2 {
     		LoadingTimer.getDimensions();
     	}
     }
-
 }
